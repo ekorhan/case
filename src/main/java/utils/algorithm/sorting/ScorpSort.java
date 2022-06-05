17f00bedf28800bf1a456aa59f2a4b79bb70bbef
Enes Korhan <enes.korhan@etiya.com>
2022-06-05 03:13:09 +0300

    Scorp sorting guarantees that worst time complexity can be O(n*m).

--- src/main/java/utils/algorithm/sorting/ScorpSort.java
+++ src/main/java/utils/algorithm/sorting/ScorpSort.java
@@ -2,69 +2,59 @@ package utils.algorithm.sorting;
 
 import entity.dto.PostDto;
 
-import java.util.ArrayList;
-import java.util.LinkedHashSet;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 
 public class ScorpSort implements ISortAlgorithm {
-
-    private List<PostDto> merge(List<List<PostDto>> input, int pointer, int index, List<PostDto> response, int lastAddedIndex) {
-        PostDto incomingPost = input.get(pointer).get(index);
-        List<PostDto> newResponse = new ArrayList<>();
-        PostDto lastVal = null;
-        if (index != 0) {
-            newResponse.addAll(response.subList(0, lastAddedIndex + 1));
-            response = response.subList(lastAddedIndex + 1, response.size());
-        }
-        for (int i = 0; i < response.size(); i++) {
-            PostDto curr = response.get(i);
-            long currCreatedAt = curr.getCreatedAt();
-            long incomingPostCreatedAt = incomingPost.getCreatedAt();
-            if (currCreatedAt == incomingPostCreatedAt) {
-                if (curr.getId() < incomingPost.getId()) {
-                    newResponse.add(curr);
-                    lastVal = incomingPost;
-                } else {
-                    newResponse.add(incomingPost);
-                    newResponse.addAll(response.subList(i, response.size()));
-                    lastAddedIndex = i;
-                    lastVal = null;
-                    break;
-                }
-            } else if (currCreatedAt < incomingPostCreatedAt) {
-                newResponse.add(curr);
-                lastVal = incomingPost;
-            } else {
-                newResponse.add(incomingPost);
-                newResponse.addAll(response.subList(i, response.size()));
-                lastAddedIndex = i;
-                lastVal = null;
-                break;
-            }
-        }
-
-        if (Objects.nonNull(lastVal))
-            newResponse.add(lastVal);
-
-        index++;
-        if (index == input.get(pointer).size()) {
-            pointer++;
-            index = 0;
-        }
-        if (pointer == input.size()) {
-            return newResponse;
-        }
-        return merge(input, pointer, index, newResponse, lastAddedIndex);
-    }
-
     @Override
     public LinkedHashSet<PostDto> sort(List<List<PostDto>> input) {
-        LinkedHashSet<PostDto> postsDesc = new LinkedHashSet<>();
-        List<PostDto> postListAsc = merge(input, 1, 0, new ArrayList<>(input.get(0)), 0);
-        for (int i = postListAsc.size() - 1; i >= 0; i--) {
-            postsDesc.add(postListAsc.get(i));
+        LinkedList<PostDto> response = new LinkedList<>();
+        Map<Integer, Integer> indexMap = new HashMap<>();
+        int counter = 0;
+        //finding N(N is the sum of size of elements in input)
+        for (int inputIndex = 0; inputIndex < input.size(); inputIndex++) {
+            indexMap.put(inputIndex, 0);
+            for (PostDto ignored : input.get(inputIndex)) {
+                counter++;
+            }
         }
-        return postsDesc;
+        //--
+
+        int willBeAddedPostIndex = 0;
+        //This will loop N times.
+        while (willBeAddedPostIndex < counter) {
+            long minDate = Integer.MAX_VALUE;
+            long minId = Integer.MAX_VALUE;
+            PostDto willBeAddedPost = null;
+            //Main list index
+            int _inputIndex = 0;
+            for (int inputIndex = 0; inputIndex < input.size(); inputIndex++) {
+                List<PostDto> currentList = input.get(inputIndex);
+                if (currentList.size() == indexMap.get(inputIndex)) {
+                    continue;
+                }
+
+                PostDto currentPost = currentList.get(indexMap.get(inputIndex));
+                long createdDate = currentPost.getCreatedAt();
+                long currentId = currentPost.getId();
+                if (createdDate == minDate) {
+                    if (currentId < minId) {
+                        minId = currentId;
+                        _inputIndex = inputIndex;
+                        willBeAddedPost = currentPost;
+                    }
+                } else if (createdDate < minDate) {
+                    minDate = createdDate;
+                    _inputIndex = inputIndex;
+                    willBeAddedPost = currentPost;
+                }
+            }
+            indexMap.put(_inputIndex, indexMap.get(_inputIndex) + 1);
+            //Push method adds element to first of the list. So it sorts desc.
+            response.push(willBeAddedPost);
+            willBeAddedPostIndex++;
+        }
+
+        //The Set guarantees each element will be unique
+        return new LinkedHashSet<>(response);
     }
 }
